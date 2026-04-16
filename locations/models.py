16547cff9c33698e6555dfa2e9d4b627288935b7
Georgi Horozov | GOPET TRANS <georgi.horozov@gopettrans.com>
2026-04-16 14:02:11 +0300

    Created third model locations

---
+++ locations/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+from trucks.models import Truck
+
+
+class Location(models.Model):
+    latitude = models.DecimalField(max_digits=5, decimal_places=2)
+    longitude = models.DecimalField(max_digits=5, decimal_places=2)
+    truck = models.ForeignKey(Truck, on_delete=models.CASCADE)
