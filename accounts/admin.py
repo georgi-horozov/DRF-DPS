73864fa1d0841a6abb97e243823ee6f4dcddc65e
Georgi Horozov | GOPET TRANS <georgi.horozov@gopettrans.com>
2026-04-17 15:02:23 +0300

    Added different logic for admin in accounts and trucks

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,16 @@
+# from django.contrib import admin
+
+# from .models import CustomUserModel
+
+# admin.site.register(CustomUserModel)
 from django.contrib import admin
+from django.contrib.auth import admin as auth_admin
+from django.contrib.auth import get_user_model
 
-from .models import CustomUserModel
+UserModel = get_user_model()
 
-admin.site.register(CustomUserModel)
+@admin.register(UserModel)
+class AppUserAdmin(auth_admin.UserAdmin):
+    model = UserModel
+    list_display = ('id', 'email')
+    ordering = ('id',)
--- trucks/admin.py
+++ trucks/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
 from .models import Truck
 
-admin.site.register(Truck)
+# admin.site.register(Truck)
+
+UserModel = get_user_model()
+
+@admin.register(Truck)
+class TruckAdmin(admin.ModelAdmin):
+    list_display = ('id', 'plate_number', 'owner')
+    ordering = ('id',)
